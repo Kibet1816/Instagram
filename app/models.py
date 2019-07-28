8198df073b9f80c2586a536799bb58d9524a0fe2
Denis Kibet <kibet1816@gmail.com>
2019-07-28 14:15:20 +0300

    Create profile model class

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    """
+    Image model class
+    """
+    image = models.ImageField(upload_to = 'media/')
+    image_name = models.CharField(max_length=60)
+    image_caption = models.TextField()
+    likes = models.IntegerField(default=0)
+    comments = models.TextField()
+
+    
+class Profile(models.Model):
+    """
+    Profile model class
+    """
+    photo = models.ImageField(upload_to='media/')
+    bio = models.TextField()
