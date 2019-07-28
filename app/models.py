47c400805d558721528ade4acbefb986b86cd8fb
Denis Kibet <kibet1816@gmail.com>
2019-07-28 15:16:36 +0300

    Add login.html page

--- app/models.py
+++ app/models.py
@@ -9,7 +9,11 @@ class Image(models.Model):
     image_name = models.CharField(max_length=60)
     image_caption = models.TextField()
     likes = models.IntegerField(default=0)
-    comments = models.TextField()
+    comments = models.TextField(blank=True)
+
+    @classmethod
+    def all_images(cls):
+        images = cls.objects.all()
 
     
 class Profile(models.Model):
--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Image
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     """
     View function to render the homepage
     """
     return render(request,'all-templates/index.html')
+
+@login_required(login_url='/accounts/login/')
+def display_images(request):
+    images = Image.all_images()
+    return render(request,'all-templates/index.html',{"images":images})
